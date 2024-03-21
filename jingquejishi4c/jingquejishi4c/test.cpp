46700597adb78cde96c49c936eb14b90fbbb3662
Gao Yuge <1463948484@qq.com>
2024-03-21 19:38:31 +0800

    add jingquejishi4c

---
+++ jingquejishi4c/jingquejishi4c/test.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <windows.h> // 包含 Windows 平台特定的头文件，用于性能计数器
+#include <stdlib.h> // 包含标准库函数的头文件
+
+using namespace std;
+void mytest(int n, int* a, int& sum) {
+	for (int m = n; m > 1; m /= 2) {// log(n)个步骤
+		for (int i = 0; i < m / 2; i++)
+			a[i] = a[i * 2] + a[i * 2 + 1]; // 相邻元素相加连续存储到数组最前面
+		// a[0]为最终结果
+	}
+	sum = a[0];
+}
+
+int main() {
+	long long head, tail, freq; // 声明三个长整型变量，用于计时
+
+	int n = 700;
+	int c;
+	int* a = new int[700];
+	for (int i = 0; i < 700; i++) {
+		a[i] = i;
+	}
+	// 获取性能计数器的频率，类似于 CLOCKS_PER_SEC
+	QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+
+	// 获取开始时间
+	QueryPerformanceCounter((LARGE_INTEGER*)&head);
+
+	for (int i = 0; i < 10000; i++) {
+		c = 0;
+		mytest(n, a, c);
+	}
+	// 获取结束时间
+	QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+
+	// 输出计算时间，单位为毫秒
+	cout << "Col: " << (tail - head) * 1000.0 / freq << "ms" << endl;
+
+	delete[]a;
+}
